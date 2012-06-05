6bc50a18e5bf6dd63e7f69299e0954c9702d2793
ischmittis <ischmittis@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-05 12:31:24 +0000

    Created Tag 1.13.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dynaflex/tags/1.13.2@63117 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "dynaflex"
+# Extension Manager/Repository config file for ext "dynaflex".
 #
-# Auto generated 17-08-2007 20:29
+# Auto generated 05-06-2012 14:29
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Dynamic Flexforms',
 	'description' => 'An extension for manipulating the TCA at runtime',
 	'category' => 'be',
-	'author' => 'Thomas Hempel',
-	'author_email' => 'thomas@typo3-unleashed.net',
+	'author' => 'Thomas Hempel, Ingo Schmitt',
+	'author_email' => 'thomas@typo3-unleashed.net, is@markeing-factory.de',
 	'shy' => '',
 	'dependencies' => '',
 	'conflicts' => '',
@@ -28,9 +28,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'author_company' => 'n@work',
-	'version' => '1.13.0',
-	'_md5_values_when_last_written' => 'a:6:{s:18:"class.dynaflex.php";s:4:"ea7b";s:31:"class.tx_dynaflex_callhooks.php";s:4:"4a9c";s:32:"class.ux_t3lib_flexformtools.php";s:4:"f80d";s:12:"ext_icon.gif";s:4:"11d8";s:17:"ext_localconf.php";s:4:"91c3";s:14:"doc/manual.sxw";s:4:"89c5";}',
+	'author_company' => '',
+	'version' => '1.13.2',
+	'_md5_values_when_last_written' => 'a:6:{s:18:"class.dynaflex.php";s:4:"3b77";s:31:"class.tx_dynaflex_callhooks.php";s:4:"6ca4";s:32:"class.ux_t3lib_flexformtools.php";s:4:"f80d";s:12:"ext_icon.gif";s:4:"11d8";s:17:"ext_localconf.php";s:4:"91c3";s:14:"doc/manual.sxw";s:4:"89c5";}',
 	'constraints' => array(
 		'depends' => array(
 		),
